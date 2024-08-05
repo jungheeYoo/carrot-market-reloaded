dd3f9376c817b899e8f57325e07929dda0e730b6
Joy <jungheeyoo.joy@gmail.com>
2024-08-05 18:52:10 +0900

    useFormStatus

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -80,9 +80,64 @@
 //   );
 // }
 
+// //-----------------------------------------------------
+// // 5-1
+// // Server Actions
+
+// import FormButton from '@/components/form-btn';
+// import FormInput from '@/components/form-input';
+// import SocialLogin from '@/components/social-login';
+
+// // Create Account Screen
+// export default function Login() {
+//   // Server action
+//   async function handleForm(formData: FormData) {
+//     // const handleForm = async() => {
+//     // use server는 이 함수가 서버에서만 실행되도록 만들어 줌
+//     // 이 함수는 비동기 함수여야함
+//     'use server';
+//     console.log(formData.get('email'), formData.get('password'));
+//     console.log('i run in the server baby!');
+//   }
+//   return (
+//     <div className="flex flex-col gap-10 py-8 px-6">
+//       <div className="flex flex-col gap-2 *:font-medium">
+//         <h1 className="text-2xl">안녕하세요!</h1>
+//         <h2 className="text-xl">Log in with email and password.</h2>
+//       </div>
+//       <form action={handleForm} className="flex flex-col gap-3">
+//         <FormInput
+//           name="email"
+//           type="email"
+//           placeholder="Email"
+//           required
+//           errors={[]}
+//         />
+//         <FormInput
+//           name="password"
+//           type="password"
+//           placeholder="Password"
+//           required
+//           errors={[]}
+//         />
+//         <FormButton loading={false} text="Log in" />
+//       </form>
+//       <SocialLogin />
+//     </div>
+//   );
+// }
+
+// // NestJS 는 POST method를 위한 route 핸들러를 만든다는 뜻
+// // 넘겨진 데이터 가져오는 방법
+// // Server action 을 만드는 순간, () 여기서 데이터를 받을 수 있다
+// // 데이터 타입은 FormData
+// // 이건 FormData constructor 내부에서 오는 것
+// // 이름은 상관없고 타입은 FormData 이어야 함
+
 //-----------------------------------------------------
-// 5-1
-// Server Actions
+// 5-2
+// useFormStatus
+// Server Action 경과와 UI가 서로 소통하는 방법
 
 import FormButton from '@/components/form-btn';
 import FormInput from '@/components/form-input';
@@ -92,12 +147,9 @@ import SocialLogin from '@/components/social-login';
 export default function Login() {
   // Server action
   async function handleForm(formData: FormData) {
-    // const handleForm = async() => {
-    // use server는 이 함수가 서버에서만 실행되도록 만들어 줌
-    // 이 함수는 비동기 함수여야함
     'use server';
-    console.log(formData.get('email'), formData.get('password'));
-    console.log('i run in the server baby!');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log('logged in!');
   }
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -120,16 +172,9 @@ export default function Login() {
           required
           errors={[]}
         />
-        <FormButton loading={false} text="Log in" />
+        <FormButton text="Log in" />
       </form>
       <SocialLogin />
     </div>
   );
 }
-
-// NestJS 는 POST method를 위한 route 핸들러를 만든다는 뜻
-// 넘겨진 데이터 가져오는 방법
-// Server action 을 만드는 순간, () 여기서 데이터를 받을 수 있다
-// 데이터 타입은 FormData
-// 이건 FormData constructor 내부에서 오는 것
-// 이름은 상관없고 타입은 FormData 이어야 함
--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,19 +1,51 @@
+// //-----------------------------------------------------
+// // 4-2
+// // Form Components
+
+// import { useFormStatus } from 'react-dom';
+
+// interface FormButtonProps {
+//   loading: boolean;
+//   text: string;
+// }
+
+// export default function FormButton({ loading, text }: FormButtonProps) {
+//   return (
+//     <button
+//       disabled={loading}
+//       className="primary-btn h-10 disabled:bg-neutral-400  disabled:text-neutral-300 disabled:cursor-not-allowed"
+//     >
+//       {loading ? '로딩 중' : text}
+//     </button>
+//   );
+// }
+
 //-----------------------------------------------------
-// 4-2
-// Form Components
+// 5-2
+// useFormStatus
+// Server Action 경과와 UI가 서로 소통하는 방법
+
+'use client';
+
+import { useFormStatus } from 'react-dom';
 
 interface FormButtonProps {
-  loading: boolean;
   text: string;
 }
 
-export default function FormButton({ loading, text }: FormButtonProps) {
+export default function FormButton({ text }: FormButtonProps) {
+  // useFormStatus
+  const { pending } = useFormStatus();
   return (
+    // 이 버튼은 form이 pending 상태라면 비활성화 됨
     <button
-      disabled={loading}
+      disabled={pending}
       className="primary-btn h-10 disabled:bg-neutral-400  disabled:text-neutral-300 disabled:cursor-not-allowed"
     >
-      {loading ? '로딩 중' : text}
+      {pending ? '로딩 중' : text}
     </button>
   );
 }
+
+// loading: boolean; 은 필요 없다
+// 이제는 hook이 form의 로딩 상태를 알려줌
