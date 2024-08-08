282cdb59a10768982258abe179d397a83c411fa4
Joy <jungheeyoo.joy@gmail.com>
2024-08-09 01:21:50 +0900

    7-4. Relationships

--- app/page.tsx
+++ app/page.tsx
@@ -25,14 +25,46 @@
 //   );
 // }
 
+// //-----------------------------------------------------
+// // 7-2
+// // Prisma Client
+// // Client 사용 방법
+
+// import Link from 'next/link';
+// // DB 파일을 이런식으로 불러오면 새로고침할 때, 최소 한 번 실행되도록 할 수 있다
+// // import '@/lib/db';
+
+// export default function Home() {
+//   return (
+//     <div className="flex flex-col items-center justify-between min-h-screen p-6">
+//       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+//         <span className="text-9xl">🥕</span>
+//         <h1 className="text-4xl">당근</h1>
+//         <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+//       </div>
+//       <div className="flex flex-col items-center gap-3 w-full">
+//         <Link href="/create-account" className="primary-btn text-lg py-2.5 ">
+//           시작하기
+//         </Link>
+//         <div className="flex gap-2">
+//           <span>이미 계정이 있나요?</span>
+//           <Link href="/login" className="hover:underline">
+//             로그인
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 //-----------------------------------------------------
-// 7-2
-// Prisma Client
-// Client 사용 방법
+// 7-4
+// Relationships
+// SMSToken 모델 만들기
+// 이 모델은 User 모델과 연결 됨. 그리고 SMS 인증을 위해서 사용할 것임
 
 import Link from 'next/link';
-// DB 파일을 이런식으로 불러오면 새로고침할 때, 최소 한 번 실행되도록 할 수 있다
-// import '@/lib/db';
+import '@/lib/db';
 
 export default function Home() {
   return (
--- lib/db.ts
+++ lib/db.ts
@@ -1,27 +1,52 @@
+// //-----------------------------------------------------
+// // 7-2
+// // Prisma Client
+// // Client 사용 방법
+
+// import { PrismaClient } from '@prisma/client';
+
+// const db = new PrismaClient();
+
+// // async function test() {
+// //   const users = await db.user.findMany({
+// //     where: {
+// //       username: {
+// //         contains: 'est',
+// //       },
+// //     },
+// //   });
+// //   console.log(users);
+// // }
+
+// // test();
+
+// export default db;
+
+// // Typescript를 사용해서 만들었고, 그건 SQL로서 DB로 전달 되었음
+// // DB는 SQL 객체로 응답했다
+// // 그러면 Prisma는 그걸 Typescript 객체로 변경한다
+
 //-----------------------------------------------------
-// 7-2
-// Prisma Client
-// Client 사용 방법
+// 7-4
+// Relationships
+// SMSToken 모델 만들기
+// 이 모델은 User 모델과 연결 됨. 그리고 SMS 인증을 위해서 사용할 것임
 
 import { PrismaClient } from '@prisma/client';
 
 const db = new PrismaClient();
 
-// async function test() {
-//   const users = await db.user.findMany({
-//     where: {
-//       username: {
-//         contains: 'est',
-//       },
-//     },
-//   });
-//   console.log(users);
-// }
-
-// test();
+async function test() {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 2,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+test();
 
 export default db;
-
-// Typescript를 사용해서 만들었고, 그건 SQL로서 DB로 전달 되었음
-// DB는 SQL 객체로 응답했다
-// 그러면 Prisma는 그걸 Typescript 객체로 변경한다
