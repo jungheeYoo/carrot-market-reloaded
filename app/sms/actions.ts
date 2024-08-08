28198394d27b07d5142c06f325ba26c01f9be672
Joy <jungheeyoo.joy@gmail.com>
2024-08-08 18:39:14 +0900

    6-8. SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -5,26 +5,85 @@
 
 // export async function smsVerification(prevState: any, formData: FormData) {}
 
+// //-----------------------------------------------------
+// // 6-7
+// // Coerce
+// 'use server';
+
+// import { z } from 'zod';
+// import validator from 'validator';
+
+// const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
+// // coerce(강제)
+// // 이걸 쓰면 유저가 입력한 string을 number로 변환한다
+// const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+// export async function smsLogin(prevState: any, formData: FormData) {
+//   console.log(typeof formData.get('token')); // string : 숫자를 입력 하지만 사실은 문자
+//   console.log(typeof tokenSchema.parse(formData.get('token'))); // number 로 변환됨
+// }
+
+// // SMS 로그인 프로세스는 두 단계로 되어 있음
+// // 1. 유저에게 전화번호 input만 보여주고, token을 보냄
+// // 2. 인증번호 input을 보여주고, 거기서 token을 인증함
+// // npm install validator
+
 //-----------------------------------------------------
-// 6-7
-// Coerce
+// 6-8
+// SMS Validation
+// interactive form
+
 'use server';
 
 import { z } from 'zod';
 import validator from 'validator';
+import { redirect } from 'next/navigation';
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, 'ko-KR'),
+    'Wrong phone format'
+  );
 
-// coerce(강제)
-// 이걸 쓰면 유저가 입력한 string을 number로 변환한다
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
-  console.log(typeof formData.get('token')); // string : 숫자를 입력 하지만 사실은 문자
-  console.log(typeof tokenSchema.parse(formData.get('token'))); // number 로 변환됨
+interface ActionState {
+  token: boolean;
 }
 
-// SMS 로그인 프로세스는 두 단계로 되어 있음
-// 1. 유저에게 전화번호 input만 보여주고, token을 보냄
-// 2. 인증번호 input을 보여주고, 거기서 token을 인증함
-// npm install validator
+export async function smsLogIn(prevState: ActionState, formData: FormData) {
+  const phone = formData.get('phone');
+  const token = formData.get('token');
+  // 잘못된 전화번호를 입력하면 validation이 실패하고 token false를 return
+  // prevState.token이 false인 경우, 즉 전화번호를 입력 받고 있다는 것
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      console.log(result.error.flatten());
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+    // token이 true 경우, token을 받고 있다는 것
+  } else {
+    const result = tokenSchema.safeParse(token); // 토큰 검증
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect('/');
+    }
+  }
+}
+
+// 이 initial state는, 이 함수를 최초 호출할 때의 prevState 값이 된다
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -71,19 +71,61 @@
 //   );
 // }
 
+// //-----------------------------------------------------
+// // 6-7
+// // Coerce
+// 'use client';
+
+// import Button from '@/components/button';
+// import Input from '@/components/input';
+// import { useFormState } from 'react-dom';
+// import { smsLogin } from './actions';
+
+// // Create Account Screen
+// export default function SMSLogin() {
+//   const [state, dispatch] = useFormState(smsLogin, null);
+//   return (
+//     <div className="flex flex-col gap-10 py-8 px-6">
+//       <div className="flex flex-col gap-2 *:font-medium">
+//         <h1 className="text-2xl">SMS Login</h1>
+//         <h2 className="text-xl">Verify your phone number.</h2>
+//       </div>
+//       <form action={dispatch} className="flex flex-col gap-3">
+//         <Input name="phone" type="text" placeholder="Phone number" required />
+//         <Input
+//           name="token"
+//           type="number"
+//           placeholder="Verification code"
+//           required
+//           min={100000}
+//           max={999999}
+//         />
+//         <Button text="Verify" />
+//       </form>
+//     </div>
+//   );
+// }
+
 //-----------------------------------------------------
-// 6-7
-// Coerce
+// 6-8
+// SMS Validation
+// interactive form
+
 'use client';
 
 import Button from '@/components/button';
 import Input from '@/components/input';
 import { useFormState } from 'react-dom';
-import { smsLogin } from './actions';
+import { smsLogIn } from './actions';
+
+const initialState = {
+  token: false,
+  error: undefined,
+};
 
 // Create Account Screen
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogIn, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -91,17 +133,36 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="text" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-          min={100000}
-          max={999999}
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          // 유저가 전화번호를 바꾸는 걸 방지하기 위해서 여기에 붙임
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        {/* state.token이 true이면 '토큰 인증하기', false이면 '인증 문자 보내기' */}
+        <Button
+          text={state.token ? 'Verify Token' : 'Send Veriffication SMS'}
         />
-        <Button text="Verify" />
       </form>
     </div>
   );
 }
+
+// 두 번째는 useFormState hook의 initial state이다 => null
+
+// 이 페이지가 처음 render 되면, state.token의 값은 false가 될 것임
+// 이 말은 여기서 input을 숨기는 데 사용할 수 있다는 것
+// state.token이 true이면 input을 보여주고, false이면 null
