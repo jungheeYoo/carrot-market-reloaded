45c29943dace530a8949d3d7554036555b71c9f9
Joy <jungheeyoo.joy@gmail.com>
2024-08-11 18:59:54 +0900

    8-9. Middleware

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+//-----------------------------------------------------
+// 8-9
+// Middleware
+
+import { NextRequest } from 'next/server';
+import getSession from './lib/session';
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  console.log(session);
+  if (request.nextUrl.pathname === '/profile')
+    return Response.redirect(new URL('/', request.url));
+}
