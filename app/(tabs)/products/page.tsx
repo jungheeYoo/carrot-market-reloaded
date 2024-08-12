5ffa73bee5abbb55bf396bbf2b6deb39d78931b4
Joy <jungheeyoo.joy@gmail.com>
2024-08-12 21:34:20 +0900

    10-2. Skeletons

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,9 +1,28 @@
+// // --------------------------------------------------------
+// // Products
+// // 10-1
+// // Tab Bar
+
+// export default function products() {
+//   return (
+//     <div>
+//       <h1 className="text-white text-4xl">Products!</h1>
+//     </div>
+//   );
+// }
+
 // --------------------------------------------------------
 // Products
-// 10-1
-// Tab Bar
+// 10-2
+// Skeletons
+// products page 작업
 
-export default function products() {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
+
+export default async function products() {
+  const products = await getProducts();
   return (
     <div>
       <h1 className="text-white text-4xl">Products!</h1>
