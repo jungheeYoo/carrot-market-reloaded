862dcf9be00dc9d7d3c226140e7e6f11cb86010c
Joy <jungheeyoo.joy@gmail.com>
2024-08-13 20:11:08 +0900

    10-10. Recap, style 삭제

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -59,10 +59,94 @@
 //   );
 // }
 
+// // --------------------------------------------------------
+// // Products
+// // 10-9
+// // Infinite Scrolling
+
+// 'use client';
+
+// import { InitialProducts } from '@/app/(tabs)/products/page';
+// import ListProduct from './list-product';
+// import { useEffect, useRef, useState } from 'react';
+// import getMoreProducts from '@/app/(tabs)/products/actions';
+
+// // tabs products에 가서 initialProducts에 마우스 대서 복사해서 가져오거나
+// // interface ProductListProps {
+// //   id: number;
+// //   title: string;
+// //   price: number;
+// //   photo: string;
+// //   created_at: Date;
+// // }[]
+
+// interface ProductListProps {
+//   initialProducts: InitialProducts;
+// }
+
+// export default function ProductList({ initialProducts }: ProductListProps) {
+//   const [products, setProducts] = useState(initialProducts);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [page, setPage] = useState(0);
+//   const [isLastPage, setIsLastPage] = useState(false);
+//   const trigger = useRef<HTMLSpanElement>(null);
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       async (
+//         entries: IntersectionObserverEntry[],
+//         observer: IntersectionObserver
+//       ) => {
+//         const element = entries[0];
+//         if (element.isIntersecting && trigger.current) {
+//           observer.unobserve(trigger.current);
+//           setIsLoading(true);
+//           const newProducts = await getMoreProducts(page + 1);
+//           if (newProducts.length !== 0) {
+//             setPage((prev) => prev + 1);
+//             setProducts((prev) => [...prev, ...newProducts]);
+//           } else {
+//             setIsLastPage(true);
+//           }
+//           setIsLoading(false);
+//         }
+//       },
+//       {
+//         threshold: 1.0,
+//         rootMargin: '0px 0px -100px 0px',
+//       }
+//     );
+//     if (trigger.current) {
+//       observer.observe(trigger.current);
+//     }
+//     return () => {
+//       observer.disconnect();
+//     };
+//   }, [page]);
+
+//   return (
+//     <div className="p-5 flex flex-col gap-5">
+//       {products.map((product) => (
+//         <ListProduct key={product.id} {...product} />
+//       ))}
+//       {!isLastPage ? (
+//         <span
+//           ref={trigger}
+//           style={{
+//             marginTop: `${page + 1 * 900}vh`,
+//           }}
+//           className=" mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+//         >
+//           {isLoading ? '로딩 중' : 'Load more'}
+//         </span>
+//       ) : null}
+//     </div>
+//   );
+// }
+
 // --------------------------------------------------------
 // Products
-// 10-9
-// Infinite Scrolling
+// 10-10
+// Recap
 
 'use client';
 
@@ -131,10 +215,7 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {!isLastPage ? (
         <span
           ref={trigger}
-          style={{
-            marginTop: `${page + 1 * 900}vh`,
-          }}
-          className=" mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? '로딩 중' : 'Load more'}
         </span>
