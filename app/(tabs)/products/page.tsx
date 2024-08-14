7992f385082854885a53372f4cdd0cb22f2dbf7a
Joy <jungheeyoo.joy@gmail.com>
2024-08-15 00:20:36 +0900

    13-1. nextCache

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -154,18 +154,72 @@
 //   );
 // }
 
+// // --------------------------------------------------------
+// // Caching
+// // 13-0
+// // Introduction
+
+// import ProductList from '@/components/product-list';
+// import db from '@/lib/db';
+// import { PlusIcon } from '@heroicons/react/24/solid';
+// import { Prisma } from '@prisma/client';
+// import Link from 'next/link';
+
+// async function getInitialProducts() {
+//   const products = await db.product.findMany({
+//     select: {
+//       title: true,
+//       price: true,
+//       created_at: true,
+//       photo: true,
+//       id: true,
+//     },
+//     /* take: 1, */
+//     orderBy: {
+//       created_at: 'desc',
+//     },
+//   });
+//   return products;
+// }
+
+// export type InitialProducts = Prisma.PromiseReturnType<
+//   typeof getInitialProducts
+// >;
+
+// export default async function products() {
+//   const initialProducts = await getInitialProducts();
+//   return (
+//     <div>
+//       <ProductList initialProducts={initialProducts} />
+//       <Link
+//         href="/products/add"
+//         className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+//       >
+//         <PlusIcon className="size-10" />
+//       </Link>
+//     </div>
+//   );
+// }
+
 // --------------------------------------------------------
 // Caching
-// 13-0
-// Introduction
+// 13-1
+// nextCache
+
+// nextCache 함수 사용하기
 
 import ProductList from '@/components/product-list';
 import db from '@/lib/db';
 import { PlusIcon } from '@heroicons/react/24/solid';
 import { Prisma } from '@prisma/client';
+import { unstable_cache as nextCache } from 'next/cache';
 import Link from 'next/link';
 
+const getCachedProducts = nextCache(getInitialProducts, ['home-products']);
+
 async function getInitialProducts() {
+  console.log('hit!!!');
+
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -187,7 +241,7 @@ export type InitialProducts = Prisma.PromiseReturnType<
 >;
 
 export default async function products() {
-  const initialProducts = await getInitialProducts();
+  const initialProducts = await getCachedProducts();
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
