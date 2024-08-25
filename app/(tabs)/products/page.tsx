fc941c2e893107ac875665f65eb12788d7f7ab6e
Joy <jungheeyoo.joy@gmail.com>
2024-08-25 14:46:39 +0900

    13-7. Route Segment Config

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -380,12 +380,71 @@
 //   );
 // }
 
+// // --------------------------------------------------------
+// // Caching
+// // 13-6
+// // Production Cache
+
+// // Next.js가 route를 어떻게 cache하는지
+
+// import ProductList from '@/components/product-list';
+// import db from '@/lib/db';
+// import { PlusIcon } from '@heroicons/react/24/solid';
+// import { Prisma } from '@prisma/client';
+// import { unstable_cache as nextCache, revalidatePath } from 'next/cache';
+// import Link from 'next/link';
+
+// const getCachedProducts = nextCache(getInitialProducts, ['home-products']);
+
+// async function getInitialProducts() {
+//   console.log('hit!!!');
+
+//   const products = await db.product.findMany({
+//     select: {
+//       title: true,
+//       price: true,
+//       created_at: true,
+//       photo: true,
+//       id: true,
+//     },
+//     /* take: 1, */
+//     orderBy: {
+//       created_at: 'desc',
+//     },
+//   });
+//   return products;
+// }
+
+// export type InitialProducts = Prisma.PromiseReturnType<
+//   typeof getInitialProducts
+// >;
+
+// export default async function products() {
+//   const initialProducts = await getInitialProducts();
+//   const revalidate = async () => {
+//     'use server';
+//     revalidatePath('/products');
+//   };
+//   return (
+//     <div>
+//       <ProductList initialProducts={initialProducts} />
+//       <form action={revalidate}>
+//         <button>Revalidate</button>
+//       </form>
+//       <Link
+//         href="/products/add"
+//         className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+//       >
+//         <PlusIcon className="size-10" />
+//       </Link>
+//     </div>
+//   );
+// }
+
 // --------------------------------------------------------
 // Caching
-// 13-6
-// Production Cache
-
-// Next.js가 route를 어떻게 cache하는지
+// 13-7
+// Route Segment Config
 
 import ProductList from '@/components/product-list';
 import db from '@/lib/db';
@@ -393,6 +452,7 @@ import { PlusIcon } from '@heroicons/react/24/solid';
 import { Prisma } from '@prisma/client';
 import { unstable_cache as nextCache, revalidatePath } from 'next/cache';
 import Link from 'next/link';
+import { title } from 'process';
 
 const getCachedProducts = nextCache(getInitialProducts, ['home-products']);
 
@@ -418,6 +478,13 @@ async function getInitialProducts() {
 export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
 >;
+
+export const metadata = {
+  title: 'Home',
+};
+
+// export const dynamic = 'force-dynamic';
+export const revalidate = 60;
 
 export default async function products() {
   const initialProducts = await getInitialProducts();
