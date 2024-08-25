90cc152e760d78f165a2cff0cc1ceecbd8d049ae
Joy <jungheeyoo.joy@gmail.com>
2024-08-25 17:57:14 +0900

    13-11. dynamicParams

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -641,10 +641,161 @@
 //   );
 // }
 
+// // --------------------------------------------------------
+// // Caching
+// // 13-10
+// // generateStaticParams
+
+// import db from '@/lib/db';
+// import { formatToWon } from '@/lib/utils';
+// import { UserIcon } from '@heroicons/react/24/solid';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { notFound } from 'next/navigation';
+// import { unstable_cache as nextCache, revalidateTag } from 'next/cache';
+
+// // 사용자가 소유자인지 아닌지 확인
+// async function getIsOwner(userId: number) {
+//   // const session = await getSession();
+//   // if (session.id) {
+//   //   return session.id === userId;
+//   // }
+//   return false;
+// }
+
+// // 상세 화면에 탭바가 나오길 원하지 않아 products 폴더 따로 만듦
+// async function getProduct(id: number) {
+//   console.log('product');
+//   // await new Promise((resolve) => setTimeout(resolve, 10000));
+//   const product = await db.product.findUnique({
+//     where: {
+//       id: id,
+//     },
+//     include: {
+//       user: {
+//         select: {
+//           username: true,
+//           avatar: true,
+//         },
+//       },
+//     },
+//   });
+//   // console.log(product);
+//   return product;
+// }
+
+// const getCachedProduct = nextCache(getProduct, ['product-detail'], {
+//   tags: ['product-detail', 'xxxx'],
+// });
+
+// async function getProductTitle(id: number) {
+//   console.log('title');
+//   const product = await db.product.findUnique({
+//     where: {
+//       id: id,
+//     },
+//     select: {
+//       title: true,
+//     },
+//   });
+//   return product;
+// }
+
+// const getCachedProductTitle = nextCache(getProductTitle, ['product-title'], {
+//   tags: ['product-title', 'xxxx'],
+// });
+
+// export async function generateMetadata({ params }: { params: { id: string } }) {
+//   const product = await getCachedProductTitle(Number(params.id));
+//   return {
+//     title: product?.title,
+//   };
+// }
+
+// export default async function ProductDetail({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   const id = Number(params.id);
+//   if (isNaN(id)) {
+//     return notFound();
+//   }
+//   const product = await getCachedProduct(id);
+//   if (!product) {
+//     return notFound();
+//   }
+//   const isOwner = await getIsOwner(product.userId);
+//   const revalidate = async () => {
+//     'use server';
+//     revalidateTag('xxxx');
+//   };
+//   return (
+//     <div>
+//       <div className="relative aspect-square">
+//         <Image
+//           fill
+//           className="object-cover"
+//           src={product.photo}
+//           alt={product.title}
+//         />
+//       </div>
+//       <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+//         <div className="size-10 overflow-hidden rounded-full">
+//           {product.user.avatar !== null ? (
+//             <Image
+//               src={product.user.avatar}
+//               width={40}
+//               height={40}
+//               alt={product.user.username}
+//             />
+//           ) : (
+//             <UserIcon />
+//           )}
+//         </div>
+//         <div>
+//           <h3>{product.user.username}</h3>
+//         </div>
+//       </div>
+//       <div className="p-5">
+//         <h1 className="text-2xl font-semibold">{product.title}</h1>
+//         <p>{product.description}</p>
+//       </div>
+//       <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center">
+//         <span className="font-semibold text-xl">
+//           {formatToWon(product.price)}원
+//         </span>
+//         {isOwner ? (
+//           <form action={revalidate}>
+//             <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+//               Revalidata title cache
+//             </button>
+//           </form>
+//         ) : null}
+//         <Link
+//           className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+//           href={``}
+//         >
+//           채팅하기
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export async function generateStaticParams() {
+//   const products = await db.product.findMany({
+//     select: {
+//       id: true,
+//     },
+//   });
+//   return products.map((product) => ({ id: product.id + '' }));
+// }
+
 // --------------------------------------------------------
 // Caching
-// 13-10
-// generateStaticParams
+// 13-11
+// dynamicParams
 
 import db from '@/lib/db';
 import { formatToWon } from '@/lib/utils';
@@ -782,6 +933,8 @@ export default async function ProductDetail({
     </div>
   );
 }
+
+export const dynamicParams = true;
 
 export async function generateStaticParams() {
   const products = await db.product.findMany({
